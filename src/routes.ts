879803700f00331ad8be10f367da7f621b0985fa
Ramonprog <programador.ramon@gmail.com>
2024-09-16 18:02:41 -0300

    feat: finalizando projeto

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,14 @@ import { CreateNutritionController } from "./controller/createNutritionControlle
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
   fastify.get('/teste', (request: FastifyRequest, response: FastifyReply) =>{
-    console.log('deu bom')
+    let res = "```json\n{\n  \"nome\": \"Alison Ramon\",\n  \"sexo\": \"Masculino\",\n  \"idade\": 29,\n  \"altura\": 1.72,\n  \"peso\": 82,\n  \"objetivo\": \"Perda de peso\",\n  \"refeicoes\": [\n    {\n      \"horario\": \"08:00\",\n      \"nome\": \"Café da manhã\",\n      \"alimentos\": [\n        \"1 fatia de pão integral\",\n        \"2 ovos mexidos com 1 colher de sopa de queijo cottage\",\n        \"1 banana\",\n        \"1 copo de leite desnatado\"\n      ]\n    },\n    {\n      \"horario\": \"10:30\",\n      \"nome\": \"Lanche da manhã\",\n      \"alimentos\": [\n        \"1 iogurte grego natural com 1 colher de sopa de granola\"\n      ]\n    },\n    {\n      \"horario\": \"13:00\",\n      \"nome\": \"Almoço\",\n      \"alimentos\": [\n        \"150g de frango grelhado\",\n        \"1 xícara de arroz integral\",\n        \"Salada verde com tomate e cenoura ralada\",\n        \"1 colher de sopa de azeite de oliva\"\n      ]\n    },\n    {\n      \"horario\": \"15:30\",\n      \"nome\": \"Lanche da tarde\",\n      \"alimentos\": [\n        \"1 maçã\",\n        \"1 punhado de castanhas\"\n      ]\n    },\n    {\n      \"horario\": \"19:00\",\n      \"nome\": \"Jantar\",\n      \"alimentos\": [\n        \"150g de peixe assado\",\n        \"1 xícara de brócolis cozido no vapor\",\n        \"1 batata doce pequena cozida\"\n      ]\n    },\n    {\n      \"horario\": \"21:00\",\n      \"nome\": \"Lanche antes de dormir\",\n        \"alimentos\": [\n        \"1 copo de leite de amêndoas\"\n      ]\n    }\n  ],\n  \"suplementos\": [\n    \"Proteína do soro do leite\",\n    \"Creatina\"\n  ],\n  \"macros\": {\n    \"proteinas\": \"1.2g por kg de peso corporal\",\n    \"carboidratos\": \"2g por kg de peso corporal\",\n    \"gorduras\": \"0.8g por kg de peso corporal\"\n  }\n}\n```"
+
+    try {
+      let jsonString = res.replace(/```\w*\n/g, '').replace(/\n```/g, '').trim()
+      response.send(JSON.parse(jsonString))
+    } catch (error) {
+      console.log("🚀 ~ fastify.get ~ error:", error)
+    }
 
     response.send({ok: true}) 
   })
--- src/service/createNutritionService.ts
+++ src/service/createNutritionService.ts
@@ -1,24 +1,50 @@
 import { CreateNutritionDto } from "../dto/create-nutrition.dto"
-import {GoogleGenerativeAI} from '@google/generative-ai'
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
-class CreateNutritionService{
-    async execute ({name, weight, height, age, gender, objective, level}: CreateNutritionDto) {
-      try {
-        const genAI = new GoogleGenerativeAI(process.env.API_KEY!)
-        const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
-        const response = await model.generateContent(`Em que ano o javascript foi criado`)
+class CreateNutritionService {
+  async execute({ name, weight, height, age, gender, objective, level }: CreateNutritionDto) {
+    try {
+      const genAI = new GoogleGenerativeAI(process.env.API_KEY!)
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+      const response = await model.generateContent(
+        `
+        Crie uma dieta completa para uma pessoa com nome: ${name} 
+        do sexo ${gender} com peso atual: ${weight}kg,  altura: ${height}, 
+        idade: ${age} anos e  com foco e objetivo em ${objective}, 
+        atualmente nível de atividade: ${level} 
+        e ignore qualquer outro parametro que não seja os passados, 
+        retorne em json com as respectivas propriedades, 
+        propriedade nome o nome da pessoa, 
+        propriedade sexo com sexo, propriedade idade, 
+        propriedade altura, propriedade peso, 
+        propriedade objetivo com o objetivo atual, 
+        propriedade refeições com uma array contendo 
+        dentro cada objeto sendo uma refeição da dieta e 
+        dentro de cada refeição a propriedade horário com 
+        horário da refeição, propriedade nome com nome e a 
+        propriedade alimentos com array contendo os alimentos dessa 
+        refeição e pode incluir uma propreidade como suplementos 
+        contendo array com sugestão de suplemento que é indicado 
+        para o sexo dessa pessoa e o objetivo dela, inclua uma propriedade com os macros dessa 
+        dieta com a soma dos valores de cada macro, no objeto de macros adicione o valor energetico total dessa
+        dieta, e não retorne
+        nenhuma observação alem das passadas no prompt, 
+        retorne em json e nenhuma propriedade pode ter acento.
+        `
+      )
 
-        if(response.response && response.response.candidates){
-          const jsonText = response.response.candidates[0]?.content.parts[0].text as string
-          return {data:jsonText}
-        }
-
-        return {ok:true}
-      } catch (error) {
-        console.log("🚀 ~ CreateNutritionService ~ execute ~ error:", error)
-        throw new Error('Failed to create nutrition')
+      if (response.response && response.response.candidates) {
+        const jsonText = response.response.candidates[0]?.content.parts[0].text as string
+        let jsonString = jsonText.replace(/```\w*\n/g, '').replace(/\n```/g, '').trim()
+        let jsonObject = JSON.parse(jsonString)
+        return { data: jsonObject }
       }
+
+    } catch (error) {
+      console.log("🚀 ~ CreateNutritionService ~ execute ~ error:", error)
+      throw new Error('Failed to create nutrition')
     }
+  }
 }
 
-export {CreateNutritionService}
+export { CreateNutritionService }
